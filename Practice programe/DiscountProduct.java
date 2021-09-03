cdb7a37898bb2e4ca4b47398be10af4417fe8b6b
Chitresh <chitreshkumar.bsr@gmail.com>
2021-09-03 20:36:55 +0530

    DSA Practice Day4

--- Practice programe/DiscountProduct.java
+++ Practice programe/DiscountProduct.java
@@ -3,5 +3,11 @@ import java.util.Scanner;
 public class DiscountProduct {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter product Price ");
+        int product = scanner.nextInt();
+        System.out.println("Enter giving Discount ");
+        int disc = scanner.nextInt();
+        float finalprice = ( product * disc) / 100 ;
+        System.out.println("Final price of a Product is " + finalprice);
     }
 }
