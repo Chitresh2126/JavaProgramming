2a6ebff831d542f4ea2f2a486f1312e8a4a4c761
Chitresh <chitreshkumar.bsr@gmail.com>
2021-09-05 21:08:55 +0530

    DSA Practice Day6

---
+++ Practice programe/ReverseStirng.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ReverseStirng {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String chater = scanner.nextLine();
+        char[] try1 = chater.toCharArray();
+        for (int i = try1.length-1;i >= 0; i--) System.out.print(try1[i]);
+      //  System.out.println("The reversed sentence is: " + reversed);
+    }
+
+
+
+    }
+
