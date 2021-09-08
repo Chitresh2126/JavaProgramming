e41fbae75186ddc9cf95c68cd0c5d278add744cf
Chitresh <chitreshkumar.bsr@gmail.com>
2021-09-08 21:44:40 +0530

    Pattern Question List added

---
+++ Practice programe/AirLineCodeChef.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class AirLineCodeChef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] A = new int[];
+        for (int i = 0; i < n ; i++){
+            A[i] = scanner.nextInt();
+        }
+    }
+}
