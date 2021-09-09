401456d1aa693f6d0f9da33690223c0e2802dba5
Chitresh <chitreshkumar.bsr@gmail.com>
2021-09-09 11:06:27 +0530

    Pattern Question

--- Patterns/Pattern9.java
+++ Patterns/Pattern9.java
@@ -1,7 +1,14 @@
+//Pattern 9
+//       *********
+//        *******
+//         *****
+//          ***
+//           *
+
 public class Pattern9 {
     public static void main(String[] args) {
-        for (int i = 1; i < 5; i++){
-            for (int j = 1; j < 9; j++){
+        for (int i = 1; i <= 5; i++){
+            for (int j = 1; j <= 9; j++){
                 if (j >= i && j <= 10-i){
                     System.out.print("*");
                 }
