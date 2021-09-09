3bb6f54c2029231a980fcbf84c19221c21736a93
Chitresh <chitreshkumar.bsr@gmail.com>
2021-09-09 06:02:16 +0530

    Pattern Question

--- Patterns/Pattern7.java
+++ Patterns/Pattern7.java
@@ -1,2 +1,23 @@
+//Pattern 7
+//        *****
+//        ****
+//        ***
+//        **
+//        *
+
+
 public class Pattern7 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++){
+            for (int j = 0 ; j < 5; j++){
+                if(j>=i){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
