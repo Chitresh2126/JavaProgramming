1680deb5970708d35c64a3c04d7dd484074b20bb
Chitresh <chitreshkumar.bsr@gmail.com>
2021-09-10 22:23:59 +0530

    Pattern Question

--- Patterns/Pattern4.java
+++ Patterns/Pattern4.java
@@ -1,16 +1,16 @@
-//Pattern 6
-//             *
-//            **
-//           ***
-//          ****
-//         *****
+//Pattern 4
+//        1
+//        1 2
+//        1 2 3
+//        1 2 3 4
+//        1 2 3 4 5
 
-public class Pattern6 {
+public class Pattern4 {
     public static void main(String[] args) {
         for (int i = 1; i <= 5; i++){
-            for (int j = 1 ; j <= 5; j++){
-                if(j>=6-i){
-                    System.out.print("*");
+            for (int j = 1; j <= 5; j++){
+                if(j <= i){
+                    System.out.print(j + " ");
                 }
                 else {
                     System.out.print(" ");
@@ -18,5 +18,6 @@ public class Pattern6 {
             }
             System.out.println();
         }
+
     }
 }
