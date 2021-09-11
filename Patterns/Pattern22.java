cbed7fd9f62c6ee84d8a98d436588ad126fd8e1b
Chitresh <chitreshkumar.bsr@gmail.com>
2021-09-11 08:25:10 +0530

    Pattern Question

---
+++ Patterns/Pattern22.java
@@ -0,0 +1,36 @@
+//Pattern 22
+
+//        1
+//        0 1
+//        1 0 1
+//        0 1 0 1
+//        1 0 1 0 1
+
+public class Pattern22 {
+    public static void main(String[] args) {
+        int k;
+        for (int i = 1; i <= 5; i++){
+            if (i % 2 == 0){
+                k = 0;
+            }
+            else {
+                k = 1;
+            }
+            for (int j = 1; j <= 5; j++){
+                if(j <= i){
+                    System.out.print(k + " ");
+                    if (k==1){
+                        k = 0;
+                    }
+                    else {
+                        k = 1;
+                    }
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
