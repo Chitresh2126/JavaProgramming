9896368b1b91a759c313a08bb6f4aa2849670946
Chitresh <chitreshkumar.bsr@gmail.com>
2021-09-12 07:58:51 +0530

    Pattern Question

---
+++ Patterns/Pattern13.java
@@ -0,0 +1,23 @@
+//Pattern 13
+
+//                *
+//               * *
+//              *   *
+//             *     *
+//            *********
+
+public class Pattern13 {
+    public static void main(String[] args) {
+        for (int i =1; i<=5 ; i++){
+            for (int j =1 ; j<=9 ; j++){
+                if (i==5||j==(6-i)||j==(4+i)){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
