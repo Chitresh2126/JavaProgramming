95c43563a6ead70583f9d88650b1862ca967f99c
Chitresh <chitreshkumar.bsr@gmail.com>
2021-09-12 06:06:09 +0530

    Pattern Question

---
+++ Patterns/Pattern30.java
@@ -0,0 +1,26 @@
+//Pattern 30
+//               1
+//             2 1 2
+//           3 2 1 2 3
+//          4 3 2 1 2 3 4
+//        5 4 3 2 1 2 3 4 5
+
+public class Pattern30 {
+    public static void main(String[] args) {
+
+        for (int i =1 ;i<=5 ;i++){
+            int m = i;
+            for (int j =1 ;j<=9 ;j++){
+                if (j>=6-i && j<=4+i){
+                    System.out.print(m);
+                    int i1 = (j < 5) ? m-- : m++;
+                }
+
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
